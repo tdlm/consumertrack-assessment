df5a188732a41ebc2640bd29de66d6ff2b41f2b1
Scott <scottmweaver@gmail.com>
2020-05-26 11:51:16 -0700

    Add WordPress install

--- bin/setup.sh
+++ bin/setup.sh
@@ -31,5 +31,48 @@ done
 section_header "Build Docker"
 docker-compose up -d
 
-section_header "Sleeping (zzz)"
-sleep 60 #TODO: Replace with hearbeat check of some kind
+# =====
+# Download WordPress
+# =====
+section_header "Download WordPress"
+docker-compose exec --user root php-fpm wp core download --force --allow-root
+
+# =====
+# Grant permissions
+# =====
+section_header "Grant Permissions"
+docker-compose exec --user root php-fpm chmod +x -R /var/www/html/wp-content
+
+
+# =====
+# Create WordPress config file, if necessary
+# =====
+section_header "Create WordPress Config"
+if [ ! -f wordpress/wp-config.php ]; then
+  n=0
+  until [ "$n" -ge 12 ]; do
+    CREATE_CONFIG=$(docker-compose exec --user root php-fpm wp config create \
+      --dbhost="db:3306" \
+      --dbname=wordpress \
+      --dbuser=wordpress \
+      --dbpass=wordpress \
+      --force \
+      --allow-root) && break
+
+    # [[ $CREATE_CONFIG == *"Success"* ]] && break
+    echo "Database not created yet. Sleeping 5 seconds..."
+    sleep 5
+  done
+fi
+
+# =====
+# Install WordPress
+# =====
+section_header "Install WordPress"
+docker-compose exec --user root php-fpm wp core install \
+  --url=localhost \
+  --title="Change Me" \
+  --admin_user="wordpress" \
+  --admin_email="user@domain.com" \
+  --admin_password="wordpress" \
+  --allow-root
